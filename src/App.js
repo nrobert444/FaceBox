71ed608ae429800f2c5fa076ed3788bf538600a5
Nicholas Roberto <njr444@gmail.com>
2020-08-17 12:52:15 -0700

    added clarifai api for face detection to app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
 import Rank from './components/rank/Rank'
+import FaceRecognition from './components/faceRecognition/FaceRecognition'
 import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
 import './App.css'
 
 const particlesOptions = {
@@ -17,6 +19,10 @@ const particlesOptions = {
     }
   }
 }
+
+const app = new Clarifai.App({
+  apiKey: 'e23009f9469e419a85f82f42af6e808f'
+})
 
 class App extends Component {
   constructor() {
@@ -32,6 +38,19 @@ class App extends Component {
 
   onButtonSubmit = e => {
     e.preventDefault()
+    app.models
+      .predict(
+        'a403429f2ddf4b49b307e318f00e528b',
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function (response) {
+          // do something with response
+        },
+        function (err) {
+          // there was an error
+        }
+      )
   }
 
   render() {
@@ -45,8 +64,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
-        <FaceRecognition />} */}
+        <FaceRecognition />
       </div>
     )
   }
---
+++ src/components/faceRecognition/FaceRecognition.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const FaceRecognition = () => {
+  return <div></div>
+}
+export default FaceRecognition
