a49467eec796d35d4458ec425a209a5042b9a50f
Nicholas Roberto <njr444@gmail.com>
2020-08-17 13:48:17 -0700

    added box feature around faces in images.

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
-export const FaceRecognition = ({ imageUrl }) => {
+import './FaceRecognition.css'
+export const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className='center'>
-      <img src={imageUrl} alt='face' />
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img
+          id='inputImage'
+          src={imageUrl}
+          alt='face'
+          width='500px'
+          height='auto'
+        />
+        <div
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        ></div>
+      </div>
     </div>
   )
 }
