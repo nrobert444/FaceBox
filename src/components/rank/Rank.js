9f3e74d63989c22633165fb35ee1931c82128d95
Nicholas Roberto <njr444@gmail.com>
2020-08-19 13:47:47 -0700

    updated Rank component to display accurate rank, passed loaduser prop to signin, updated response from backend to checkcorrect data being loaded.

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export const Rank = () => {
+export const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className='white f3 '>{'#5'}</div>
+      {`{name} , your current rank is...`}
+      <div className='white f1 '>{entries}</div>
     </div>
   )
 }
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -32,14 +32,16 @@ class Register extends Component {
       })
     })
       .then(response => response.json())
-      .then(data => {
-        if (data === 'success') {
+      .then(user => {
+        if (user) {
+          this.props.loadUser(user)
           this.props.onRouteChange('home')
         }
       })
   }
 
   render() {
+    const { onRouteChange } = this.props
     return (
       <article className='br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
         <main className='pa4 black-80'>
